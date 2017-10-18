3954110a0939d49e299ce6b6774c201d3d644ad3
uirapeixoto <uira.peixoto@gmail.com>
2017-10-18 00:03:27 -0200

    output-property: trocando informações entre os componetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { MeuFormComponent } from './meu-form/meu-form.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MeuFormComponent } from './meu-form/meu-form.component';
 
 import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { MeuFormModule } from './meu-form/meu-form.module';
 import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
-    InputPropertyComponent
+    InputPropertyComponent,
+    OutputPropertyComponent
   ],
   imports: [
     BrowserModule,
--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -14,7 +14,8 @@ export class DataBindingComponent implements OnInit {
   valorSalvo:string = '';
   isMouseOver:boolean = false;
 
-  nomeCurso: string = 'Angular';
+  nomeDoCurso: string = 'Angular';
+  valorInicial = 15;
   
   getValor(){
     return 1;
@@ -38,6 +39,10 @@ export class DataBindingComponent implements OnInit {
 
   onMouseOverOut(){
     this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor(evento){
+    console.log(evento.novoValor)
   }
 
   constructor() { }
