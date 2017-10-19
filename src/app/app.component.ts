8bc3840efbc1d5ad1bd9c3f700756032d6340842
uirapeixoto <uira.peixoto@gmail.com>
2017-10-19 18:21:44 -0200

    diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  valor: number = 5;
-
-  mudarValor(){
-    this.valor++; 
-   }
-
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MeuFormComponent } from './meu-form/meu-form.component';
 
-import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
-import { MeuFormModule } from './meu-form/meu-form.module';
-import { InputPropertyComponent } from './input-property/input-property.component';
-import { OutputPropertyComponent } from './output-property/output-property.component';
-import { CicloComponent } from './ciclo/ciclo.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataBindingComponent,
-    InputPropertyComponent,
-    OutputPropertyComponent,
-    CicloComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AlertModule.forRoot(),
-    MeuFormModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
