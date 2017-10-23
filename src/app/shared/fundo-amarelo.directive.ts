f8bfd8b3b700abc0eb6926c032f12cfe8b62c72c
uirapeixoto <uira.peixoto@gmail.com>
2017-10-23 17:44:51 -0200

    Criando uma diretiva de atributo: ElementRef e Rederer

--- src/app/shared/fundo-amarelo.directive.ts
+++ src/app/shared/fundo-amarelo.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
-  selector: '[appFundoAmarelo]'
+  //selector: '[fundoAmarelo]'
+  selector: 'p[fundoAmarelo]'
 })
 export class FundoAmareloDirective {
 
-  constructor() { }
+  constructor(
+    private _elementRef:ElementRef,
+    private _rederer: Renderer
+  ) {
+    //console.log(this._elementRef);
+    //this._elementRef.nativeElement.style.backgroundColor = 'yellow';
+    /*
+    Não é aconselhado utilizar o elemento DOM diretamente pois isso caracteriza falha
+    de segurança como por exemplo ataque de XSS que são ataques de Cross-site Scripting
+    leia mais em http://www.redesegura.com.br/2012/01/saiba-mais-sobre-o-cross-site-scripting-xss/
+    */ 
+    this._rederer.setElementStyle(
+      this._elementRef.nativeElement,
+      'background-color',
+      'yellow'
+    );
+   }
 
 }
