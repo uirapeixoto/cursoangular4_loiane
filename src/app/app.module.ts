04d7caa9669744bb1b2ea9c93a93772164ebbaeb
uirapeixoto <uira.peixoto@gmail.com>
2017-10-24 11:32:57 -0200

    Instalando as bibliotecas de stylo, bootstrap, pope e font-awesom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { CursosModule } from './cursos/cursos.module';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,8 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    CursosModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
