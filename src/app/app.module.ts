6e2dbc1ba7a34d5a06e61a97c5af4dd69794ae19
uirapeixoto <uira.peixoto@gmail.com>
2017-10-27 13:23:14 -0200

    Adicionando navbar no layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpModule } from '@angular/http';
 import { CursosModule } from './cursos/cursos.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
